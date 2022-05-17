db7eaad3a3a5bc5f0502175c537e3b65df23a97a
Hayley Berryhill <105015006+Hayley-rae@users.noreply.github.com>
2022-05-17 18:35:26 -0500

    added search functionality
    A city search will now find and display the city name, humidity, and windspeed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 let now = new Date();
 
-function showWeather(response) {
-  console.log(response);
+function getWeather(event) {
+  event.preventDefault();
+  let city = userInput.value;
+  let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=27c5687a16012c8b1ba45274dda2ba7e&units=metric`;
+  axios.get(weatherApi).then(displayWeather);
+}
+function displayWeather(response) {
+  let datapoint = response.data;
+  let cityName = datapoint.name;
+  let humidity = datapoint.main.humidity;
+  let wind = Math.round(datapoint.wind.speed);
+  cityElement.innerHTML = cityName;
+  humidityElement.innerHTML = `Humidity: ${humidity}%`;
+  windElement.innerHTML = `Wind: ${wind}kh`;
 }
 
-function findData(event) {
-  event.preventDefault();
-  console.log(response);
-}
 let searchForm = document.querySelector("#searchForm");
 let userInput = searchForm.querySelector("#search-input");
-searchForm.addEventListener("submit", findData);
+searchForm.addEventListener("submit", getWeather);
 
-let rightCol = document.querySelector("#col-2");
-let cityElement = rightCol.querySelector(".city");
-
-let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=&appid=27c5687a16012c8b1ba45274dda2ba7e&units=metric`;
-axios.get(weatherApi).then(showWeather);
+let conditions = document.querySelector("#col-2");
+let cityElement = conditions.querySelector(".city");
+let humidityElement = conditions.querySelector(".humidity");
+let windElement = conditions.querySelector(".wind");
