9f7c255a0b30698ee7598ca826b0e636d55cc5cd
Hayley Berryhill <105015006+Hayley-rae@users.noreply.github.com>
2022-05-19 14:32:17 -0500

    additional search features added
    search will now find and display the appropriate condition icon.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-let now = new Date();
-
 function getWeather(event) {
   event.preventDefault();
   let city = userInput.value;
-  let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=27c5687a16012c8b1ba45274dda2ba7e&units=metric`;
+  let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=27c5687a16012c8b1ba45274dda2ba7e&units=imperial`;
   axios.get(weatherApi).then(displayWeather);
 }
 function displayWeather(response) {
   let datapoint = response.data;
   let cityName = datapoint.name;
+  let temp = Math.round(datapoint.main.temp);
   let humidity = datapoint.main.humidity;
   let wind = Math.round(datapoint.wind.speed);
+  let iconId = datapoint.weather[0].icon;
+  let iconUrl = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
   cityElement.innerHTML = cityName;
   humidityElement.innerHTML = `Humidity: ${humidity}%`;
-  windElement.innerHTML = `Wind: ${wind}kh`;
+  windElement.innerHTML = `Wind: ${wind}mph`;
+  tempElement.innerHTML = `${temp}º`;
+  currentIcon.src = iconUrl;
 }
 
 let searchForm = document.querySelector("#searchForm");
@@ -24,3 +27,6 @@ let conditions = document.querySelector("#col-2");
 let cityElement = conditions.querySelector(".city");
 let humidityElement = conditions.querySelector(".humidity");
 let windElement = conditions.querySelector(".wind");
+let tempElement = document.querySelector("#current-temp");
+
+let currentIcon = document.getElementById("conditionIcon");
